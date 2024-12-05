e6657899fce5ec51ca38aea7b4e53b2bf258d526
Beniamin Sferciuc <116905787+BeniaminSferciuc@users.noreply.github.com>
2024-12-05 12:58:45 +0200

    fix bug

--- src/main/ipcs/sendAllSongs.ts
+++ src/main/ipcs/sendAllSongs.ts
@@ -1,6 +1,6 @@
 import { ipcMain } from 'electron'
 import { Lyric } from '../types'
-import { getAllDocuments } from '../db/db'
+import { getAllDocuments } from '../db/queries'
 
 export const sendAllSongs = () => {
   ipcMain.handle('send-all-songs', async (): Promise<Lyric[] | undefined> => {
